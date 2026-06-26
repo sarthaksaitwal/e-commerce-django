810253d30a31096735eea0a3b69054460d3e2c52
sarthaksaitwal <sarthaksaitwal3@gmail.com>
2026-06-26 17:35:35 +0530

    added views and url to the products

--- backend/products/views.py
+++ backend/products/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
+def index(request):
+    products = Product.objects.all()
+    return render(request,'products/index.html',{'products':products})
