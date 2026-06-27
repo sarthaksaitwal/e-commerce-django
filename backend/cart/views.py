2dd9e1b084c9484cf95f44eca4fa00d7a1bcd782
sarthaksaitwal <sarthaksaitwal3@gmail.com>
2026-06-27 20:13:16 +0530

    implemented the route for the add to cart functionality

--- backend/cart/views.py
+++ backend/cart/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
 # Create your views here.
+
+def cart_add(request):
+    print("Add to cart button clicked")
+    if request.method == "POST":
+        product_id = request.POST.get("product_id")
+        product_quantity = request.POST.get("product_quantity")
+        print("product_id : ",product_id)
+        print("quantity : ",product_quantity)
+    return JsonResponse({
+        'Message':'Add to cart button clicked'
+    })
